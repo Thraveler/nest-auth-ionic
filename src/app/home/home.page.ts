e183d832e80fb05c7c68b5bf78eda22f7cdc0b66
Daniel Méndez <daphni_mgld@hotmail.com>
2019-05-20 15:48:24 -0500

    Se realizo una aplicacion en Ionic que permite crear cuentas de usuario, simular sign in y sign out

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  public createEmail: string;
+  public createPassword: string;
+  public signInEmail: string;
+  public signInPassword: string;
+  public jwt: string;
+
+  constructor(private http: HttpClient){
+
+  }
+
+  createAccount(){
+
+    let credentials = {
+      email: this.createEmail,
+      password: this.createPassword
+    }
+
+    this.http.post('http://localhost:3000/users', credentials).subscribe((res) => {
+      console.log(res);
+    });
+
+  }
+
+  signIn(){
+
+    let credentials = {
+      email: this.signInEmail,
+      password: this.signInPassword
+    }
+
+    this.http.post('http://localhost:3000/auth', credentials).subscribe((res: any) => {
+      console.log(res);
+
+      // NOTE: This is just for testing, typically you would store the JWT in local storage and retrieve from there
+      this.jwt = res.token;
+    });
+
+  }
+
+  testRoute(){
+
+    console.log(this.jwt);
+
+    let headers = new HttpHeaders().set('Authorization', 'Bearer ' + this.jwt)
+
+    this.http.get('http://localhost:3000/users/test', {headers: headers}).subscribe((res) => {
+      console.log(res);
+    });
+
+  }
+
+  logout(){
+    this.jwt = null;
+  }
 
 }
